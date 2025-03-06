18058e4008d487eba70b7bb4c10f72c1b60309a6
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2025-03-06 21:46:52 +0100

    feat: add load csv files

--- mySQl/load_csv.py
+++ mySQl/load_csv.py
@@ -11,41 +11,46 @@ may_2022 = pd.read_csv("./Data/International sale Report.csv")
 pl_march_2021 = pd.read_csv("./Data/P  L March 2021.csv")
 sales_report = pd.read_csv("./Data/Sale Report.csv")
 
-# print(amazon_sales.head())
+print(amazon_sales.head())
 # print(cloud_warehouse.head())
 # print(expenses.head())
 # print(international_sales.head())
 # print(may_2022.head())
-# print(pl_march_2021.head())
+# print(pl_march_2021.head())fas
 # print(sales_report.head())
 
 
 
 #* creating the dim_data table extracting from all csv dataset that contains date fields
-date_frames = [amazon_sales, international_sales, may_2022, sales_report]
+# date_frames = [amazon_sales, international_sales, may_2022, sales_report]
 
-for df in date_frames:
+# for df in date_frames:
     
-    if "Date"  in df.columns:
-        df["Date"] = pd.to_datetime(df["Date"])
+#     if "Date"  in df.columns:
+#         df["Date"] = pd.to_datetime(df["Date"])
 
 
-all_dates = pd.concat([df[["Date"]] for df in date_frames if "Date" in df.columns]).drop_duplicates()
-#print(all_dates.head())
-dim_date = all_dates.copy()
+# all_dates = pd.concat([df[["Date"]] for df in date_frames if "Date" in df.columns]).drop_duplicates()
+# #print(all_dates.head())
+# dim_date = all_dates.copy()
 
-dim_date["date_id"] = dim_date["Date"].dt.strftime("%Y%m%d").astype(int)
-dim_date["year"] = dim_date["Date"].dt.year
-dim_date["month"] = dim_date["Date"].dt.month
-dim_date["day"] = dim_date["Date"].dt.day
-dim_date["quarter"] = dim_date["Date"].dt.to_period("Q").astype(str)
-#dim_date["Weekday"] = dim_date["Date"].dt.day_name()
+# dim_date["date_id"] = dim_date["Date"].dt.strftime("%Y%m%d").astype(int)
+# dim_date["year"] = dim_date["Date"].dt.year
+# dim_date["month"] = dim_date["Date"].dt.month
+# dim_date["day"] = dim_date["Date"].dt.day
+# dim_date["quarter"] = dim_date["Date"].dt.to_period("Q").astype(str)
+# #dim_date["Weekday"] = dim_date["Date"].dt.day_name()
 
-dim_date = dim_date[["date_id", "Date", "year", "month", "day", "quarter"]]
+# dim_date = dim_date[["date_id", "Date", "year", "month", "day", "quarter"]]
 
-print(dim_date.head(10))
+# #print(dim_date.head(10))
+# #* will continue rest of the dim_tables :) 
+# #* dim_product table 
 
-#* will continue rest of the dim_tables :) 
+# dim_product = pd.concat([
+    
+    
+# ])
 
 
 
