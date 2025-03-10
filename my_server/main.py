7b3832fa1f2b217839c5a4999af312b53f79ae3d
Mahmoud Ibrahim *_* <abojan.m1993@gmail.com>
2025-03-10 22:21:18 +0100

    added CORS to server to let client from different domain communicate with it

--- my_server/main.py
+++ my_server/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from my_server.endpoints.analytics_api import analytics_api
 from my_server.endpoints.test_api import test_api
 
 app = Flask(__name__)
+CORS(app)
 
 # Register the Blueprint correctly
 app.register_blueprint(test_api)
