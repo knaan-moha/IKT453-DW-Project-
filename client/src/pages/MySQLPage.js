0adbe7c1243370cef9bea7723eadd2eabc2d0ef4
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2025-04-07 15:17:24 +0200

    feat: add final mySql queries

--- client/src/pages/MySQLPage.js
+++ client/src/pages/MySQLPage.js
@@ -14,6 +14,67 @@ const mysqlQueries = [
     queryUrl:
       "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%20%20%20%20fs.customer_id%2C%0A%20%20%20%20COUNT%28fs.order_id%29%20AS%20total_orders%2C%0A%20%20%20%20Round%28SUM%28fs.amount%29%2C%203%29%20AS%20total_spent%0AFROM%20FactSales%20fs%0AGROUP%20BY%20fs.customer_id%0AORDER%20BY%20total_orders%20DESC%0ALIMIT%2010",
   },
+
+  {
+    text: "What is the distribution of cancelled and returned orders by status type?",
+    category: "Operational Efficiency",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%20%20%20%20os.status_name%20AS%20order_status%2C%0A%20%20%20%20COUNT%28fs.order_id%29%20AS%20total_orders%0AFROM%20FactSales%20fs%0AJOIN%20DimOrderStatus%20os%20ON%20fs.status_id%20%3D%20os.status_id%0AWHERE%20os.status_name%20IN%20%28%0A%20%20%20%20%27Cancelled%27%2C%0A%20%20%20%20%27Shipped%20-%20Returned%20to%20Seller%27%2C%0A%20%20%20%20%27Shipped%20-%20Rejected%20by%20Buyer%27%2C%0A%20%20%20%20%27Shipped%20-%20Lost%20in%20Transit%27%2C%0A%20%20%20%20%27Shipped%20-%20Returning%20to%20Seller%27%2C%0A%20%20%20%20%27Shipped%20-%20Damaged%27%0A%29%0AGROUP%20BY%20os.status_name%0AORDER%20BY%20total_orders%20DESC%3B%0A",
+  },
+  {
+    text: "What is the total gross sales revenue generated in April 2022 across all channels and products?",
+    category: "Sales Performance",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%09p.category%2C%20%0A%20%20%20%20f.Sales_Channel_%2C%0A%20%20%20%20SUM%28fs.amount%29%20AS%20total_sales%0AFROM%20FactSales%20fs%0AJOIN%20DimDate%20d%20ON%20fs.date_id%20%3D%20d.date_id%0AJOIN%20DimProduct%20p%20ON%20fs.product_id%20%3D%20p.product_id%20%0AJOIN%20DimFulfillment%20f%20ON%20fs.fulfillment_id%20%3D%20f.fulfillment_id%0AWHERE%20d.year%20%3D%202022%20AND%20d.month%20%3D%204%0AGROUP%20BY%20p.category%2C%20f.Sales_Channel_%3B",
+  },
+
+  {
+    text: "Which top 10 SKUs generated the highest gross sales revenue in April 2022, considering their category, size, style, and sales channel?",
+    category: "Sales Performance",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%20%20%20%20p.SKU%2C%0A%20%20%20%20p.Category%2C%0A%20%20%20%20p.Size%2C%0A%20%20%20%20p.Style%2C%0A%20%20%20%20f.Sales_Channel_%2C%0A%20%20%20%20SUM%28fs.amount%29%20AS%20total_gross_sales%0AFROM%20FactSales%20fs%0AJOIN%20DimProduct%20p%20ON%20fs.product_id%20%3D%20p.product_id%0AJOIN%20DimFulfillment%20f%20ON%20fs.fulfillment_id%20%3D%20f.fulfillment_id%0AJOIN%20DimDate%20d%20ON%20fs.date_id%20%3D%20d.date_id%0AWHERE%20d.year%20%3D%202022%20AND%20d.month%20%3D%204%0AGROUP%20BY%20p.SKU%2C%20p.Category%2C%20p.Size%2C%20p.Style%2C%20f.Sales_Channel_%0AORDER%20BY%20total_gross_sales%20DESC%0ALIMIT%2010%3B%0ASELECT%20%2A%20FROM%20%60DimProduct%60%20WHERE%2011",
+  },
+
+  {
+    text: "What are the total sales by order status?",
+    category: "Sales Performance",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%20%20%20%20os.status_name%20AS%20order_status%2C%0A%20%20%20%20COUNT%28fs.order_id%29%20AS%20total_orders%2C%0A%20%20%20%20SUM%28fs.amount%29%20AS%20total_sales%0AFROM%20FactSales%20fs%0AJOIN%20DimOrderStatus%20os%20ON%20fs.status_id%20%3D%20os.status_id%0AGROUP%20BY%20os.status_name%0AORDER%20BY%20total_sales%20DESC%3B",
+  },
+
+  {
+    text: "What is the total number of orders and gross sales revenue for each order status?",
+    category: "Sales Performance",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%20%20%20%20d.year%2C%0A%20%20%20%20d.month%2C%0A%20%20%20%20SUM%28fs.amount%29%20AS%20total_monthly_sales%0AFROM%20FactSales%20fs%0AJOIN%20DimDate%20d%20ON%20fs.date_id%20%3D%20d.date_id%0AGROUP%20BY%20d.year%2C%20d.month%0AORDER%20BY%20d.year%2C%20d.month%3B",
+  },
+
+  {
+    text: "Which products has never been sold in any report?",
+    category: "Inventory & Product Analysis",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%0A%09p.product_id%2C%20%0A%20%20%20%20p.Category%2C%20%0A%20%20%20%20p.SKU%2C%20%0A%20%20%20%20p.Size%2C%20%0A%20%20%20%20p.Style%0AFROM%20DimProduct%20p%0ALEFT%20JOIN%20FactSales%20fs%20ON%20p.product_id%20%3D%20fs.product_id%0AWHERE%20fs.product_id%20is%20NULL%3B%2",
+  },
+
+  {
+    text: "Which top 5 product sizes are the most frequently ordered among items that have been shipped?",
+    category: "Inventory & Product Analysis",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%20%20%20%20p.Size%2C%0A%20%20%20%20COUNT%28fs.order_id%29%20AS%20total_shipped_orders%0AFROM%20FactSales%20fs%0AJOIN%20DimProduct%20p%20ON%20fs.product_id%20%3D%20p.product_id%0AJOIN%20DimOrderStatus%20os%20ON%20fs.status_id%20%3D%20os.status_id%0AWHERE%20os.status_name%20LIKE%20%27Shipped%25%27%0AGROUP%20BY%20p.Size%0AORDER%20BY%20total_shipped_orders%20DESC%0ALIMIT%205%3B",
+  },
+
+  {
+    text: "Which top 10 products generated the highest total gross sales revenue across all time?",
+    category: "Inventory & Product Analysis",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%20%0A%20%20%20%20p.SKU%2C%0A%20%20%20%20p.Category%2C%0A%20%20%20%20p.Size%2C%0A%20%20%20%20p.Style%2C%0A%20%20%20%20SUM%28fs.amount%29%20AS%20total_revenue%0AFROM%20FactSales%20fs%0AJOIN%20DimProduct%20p%20ON%20fs.product_id%20%3D%20p.product_id%0AGROUP%20BY%20p.SKU%2C%20p.Category%2C%20p.Size%2C%20p.Style%0AORDER%20BY%20total_revenue%20DESC%0ALIMIT%2010%3B%0A",
+  },
+  {
+    text: "What is the total sales distribution across product categories, based on gross revenue and order volume?",
+    category: "Inventory & Product Analysis",
+    queryUrl:
+      "http://0.0.0.0:5000/analytics?DB=mysql&query=SELECT%0A%09p.Category%2C%20%0A%20%20%20%20COUNT%28fs.order_id%29%20AS%20total_orders%2C%20%0A%20%20%20%20SUM%28fs.amount%29%20AS%20total_revenue%0AFROM%20FactSales%20fs%20%0AJOIN%20DimProduct%20p%20ON%20fs.product_id%20%3D%20p.product_id%0AGROUP%20BY%20p.Category%0AORDER%20BY%20total_revenue%20DESC",
+  },
 ];
 
 const MySQLPage = () => {
--- client/src/pages/ResultPage.js
+++ client/src/pages/ResultPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -7,10 +7,17 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+import "../styles/result.css";
 
 const ResultPage = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
+
+  // Pagination state
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
   const formatINR = (value) =>
     new Intl.NumberFormat("en-IN", {
       style: "currency",
@@ -24,50 +31,84 @@ const ResultPage = () => {
     ? JSON.parse(decodeURIComponent(queryParams.get("data")))
     : null;
 
+  // Pagination handlers
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   const renderTable = () => {
     if (!data || data.length === 0) return <p>No data received</p>;
 
     const headers = Object.keys(data[0]);
 
+    const capitalizedHeaders = headers.map((header) =>
+      header
+        .split("_")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+    );
+
+    // Slice the data for current page
+    const currentPageData = data.slice(
+      page * rowsPerPage,
+      page * rowsPerPage + rowsPerPage
+    );
+
     return (
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {headers.map((header) => (
-                <TableCell
-                  key={header}
-                  sx={{
-                    fontWeight: "bold",
-                    backgroundColor: "#1976d2",
-                    color: "white",
-                  }}
-                >
-                  {header}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row, rowIndex) => (
-              <TableRow key={rowIndex}>
-                {headers.map((header) => (
-                  //Here we can same format if we do not need  to use the indian currency
-                  //<TableCell key={header}>{row[header]}</TableCell>
-                  <TableCell key={header}>
-                    {typeof row[header] === "number" &&
-                    moneyKeywords.some((keyword) =>
-                      header.toLowerCase().includes(keyword)
-                    )
-                      ? formatINR(row[header])
-                      : row[header]}
+      <Paper>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {headers.map((header, index) => (
+                  <TableCell
+                    key={header}
+                    sx={{
+                      fontWeight: "bold",
+                      backgroundColor: "#1976d2",
+                      color: "white",
+                      fontSize: "1.25rem",
+                      textTransform: "capitalize",
+                    }}
+                  >
+                    {capitalizedHeaders[index]}
                   </TableCell>
                 ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {currentPageData.map((row, rowIndex) => (
+                <TableRow key={rowIndex}>
+                  {headers.map((header) => (
+                    <TableCell key={header}>
+                      {typeof row[header] === "number" &&
+                      moneyKeywords.some((keyword) =>
+                        header.toLowerCase().includes(keyword)
+                      )
+                        ? formatINR(row[header])
+                        : row[header]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25, 50, 100]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
     );
   };
 
@@ -75,7 +116,8 @@ const ResultPage = () => {
     <div style={{ padding: "2rem" }}>
       <h2>Query Result</h2>
       <p>
-        <strong>Query:</strong> {query}
+        <strong>Query:</strong>
+        <span className="query-text">{query}</span>
       </p>
       {renderTable()}
     </div>
